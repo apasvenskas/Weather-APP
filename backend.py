198691c2bd6e00d487476a2d9f38eba086706e4a
Audrius Pasvenskas <apasvenskas@outlook.com>
2025-03-25 20:31:09 -0700

    working on accessing api data

--- backend.py
+++ backend.py
@@ -1,3 +1,19 @@
-def get_data(place, days, kind):
-    url = "api.openweathermap.org/data/2.5/forecast?id={city ID}&appid={API key}"
+import requests
+
+API_KEY = "2c3707c741c25542940df8dfcd5446cd"
+
+# data structure for accessing the stored data in API
+def get_data(place, days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json
+    filtered_data = data["list"]
+    nr_values = 8 * days
+    filtered_data = filtered_data[:nr_values]
     return data
+
+    if kind == "Temperature":
+        filtered_data = []
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo"))
