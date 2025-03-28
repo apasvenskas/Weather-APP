09e443834bc7fb203e320a730e0314eea921c464
Audrius <apasvenskas@outlook.com>
2025-03-27 19:39:41 -0700

    sky conditions added

--- backend.py
+++ backend.py
@@ -10,10 +10,13 @@ def get_data(place, days=None, kind=None):
     filtered_data = data["list"]
     nr_values = 8 * days
     filtered_data = filtered_data[:nr_values]
-    return data
 
     if kind == "Temperature":
-        filtered_data = []
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
+
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", days=3, kind="Temperature"))
