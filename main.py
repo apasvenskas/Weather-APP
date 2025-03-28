6f891246c97f461885c4e35a34c853d8bfd93ee1
Audrius <apasvenskas@outlook.com>
2025-03-27 20:47:11 -0700

    succesfully displaying sky and temp data via GUI

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ option = st.selectbox("Select data to view.", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
 
-d, t = get_data(days)
+if place:
+    filtered_data = get_data(place, days)
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y":"Temperature (C)"})
+        st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y":"Temperature (C)"})
-st.plotly_chart(figure)
+    if option == "Sky":
+        images = {"Clear":"images/clear.png", "Clouds":"images/cloud.png", "Rain":"images/rain.png", "Snow":"images/snow.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        paths = [images[condition] for condition in sky_conditions]
+        st.image(paths, width = 100)
